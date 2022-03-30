153e5c7a81fd81c0141276b94dbd0263609da650
Heiko Goretzki <heikogoretzki@gmail.com>
2022-03-30 09:34:29 +0200

    save

--- src/main/java/oop/abstraction/Main.java
+++ src/main/java/oop/abstraction/Main.java
@@ -1,6 +1,8 @@
 package oop.abstraction;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -16,6 +18,21 @@ public class Main {
             if (shape instanceof WithDiagonal withDiagonal) {
                 System.out.println("\tdiagonal length: " + withDiagonal.getDiagonalLength() + "; rounded: " + withDiagonal.getDiagonalLengthRounded());
             }
+        }
+
+//        Function<Integer, Integer> mapFunction = x -> x * x;
+
+        Stream.of(4,2,6,7)
+//                .map(mapFunction)
+                .map(new MapFunction())
+                .forEach(System.out::println); // x -> System.out.println(x);
+    }
+
+    private static class MapFunction implements Function<Integer, Integer> {
+
+        @Override
+        public Integer apply(Integer x) {
+            return x * x;
         }
     }
 }
